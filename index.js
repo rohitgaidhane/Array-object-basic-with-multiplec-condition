cc0b1925127bbbbad14b3c42f5bd8ea02758dfa2
rohitgaidhane <rohitgaidhane123@gmail.com>
2023-12-30 21:55:14 +0530

    basic aaray

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-// Import stylesheets
-import './style.css';
-
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+let arr = [
+    { name: "user1", age: 28 ,isVerified :true}, // arr[0]
+    { name: "user2", age: 25 ,isVerified :false}, // arr[1]
+    { name: "user3", age: 38 ,isVerified :true}, // arr[2]
+    { name: "user4", age: 25 ,isVerified :true}, // arr[3]
+  ];
+  
+  function filterArray() {
+    let tempArr = [];  // temp array 
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].isVerified === true || arr[i].age < 30) { 
+          // true                         //true 
+        tempArr.push(arr[i]);
+      }
+    }
+    return tempArr;
+  }
+  
+  // console.log(arr);
+  let VerfiedUserwithAgelessthan30  = filterArray();
+  console.log(VerfiedUserwithAgelessthan30,"Verfied User with Age less than 30");
+  
